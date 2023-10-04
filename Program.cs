46c0d8682fc526871198ac21d92cf0e545411a64
Daniel <Daniel@DESKTOP-KKC0TQU>
2023-10-03 21:11:37 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_7___ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Divisor de Daniel Fernando: " + i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
